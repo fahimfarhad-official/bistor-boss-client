d4b11ba51db012b819001865f1b2359c2bdceac1
Fahim Farhad <fahimfarhad@Fahims-MacBook-Air.local>
2023-05-26 00:18:46 +0600

    Test Monials section  added

--- src/Pages/Home/TestiMonials/TestiMonials.jsx
+++ src/Pages/Home/TestiMonials/TestiMonials.jsx
@@ -1,15 +1,52 @@
-import React from 'react';
-import SectionTitle from '../../../Component/SectionTitle/SectionTitle';
+import React, { useEffect, useState } from "react";
+import SectionTitle from "../../../Component/SectionTitle/SectionTitle";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Navigation, Pagination } from "swiper";
+import { Rating } from "@smastrom/react-rating";
+import '@smastrom/react-rating/style.css'
 
 const TestiMonials = () => {
-    return (
-        <div>
-            <SectionTitle
-            subHeading={'What Our Client Say'}
-            heading={'testimonials'}
-            ></SectionTitle>
-        </div>
-    );
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    fetch("/review.json")
+      .then((res) => res.json())
+      .then((data) => setReviews(data));
+  }, []);
+  return (
+    <section className="my-20">
+      <SectionTitle
+        subHeading={"What Our Client Say"}
+        heading={"testimonials"}
+      ></SectionTitle>
+      <Swiper
+        pagination={{
+          type: "fraction",
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {reviews.map((review) => (
+          <SwiperSlide
+            className="text-center
+            "
+            key={review._id}
+          >
+            <div className="my-24 mx-32">
+              <Rating className="mx-auto mb-2" style={{ maxWidth: 180 }} value={review.Rating} readOnly />
+              <p className="">{review.details}</p>
+              <h2 className="text-2xl text-orange-500 mt-3">{review.name}</h2>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
+  );
 };
 
 export default TestiMonials;
