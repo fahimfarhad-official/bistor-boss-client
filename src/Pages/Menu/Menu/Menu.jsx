36a34284a33b6e6c10ffc7058783ab01a2afb909
Fahim Farhad <fahimfarhad@Fahims-MacBook-Air.local>
2023-05-26 23:25:55 +0600

    add image parallax on menu fulder

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
-import { Helmet } from 'react-helmet-async';
+import React from "react";
+import { Helmet } from "react-helmet-async";
+import Cover from "../../Shared/Cover/Cover";
+import menuImg from "../../../assets/menu/banner3.jpg";
+import PopularManu from "../../Home/Popular Menu/PopularManu";
 
 const Menu = () => {
-    return (
-        <div>
-            <Helmet>
-                <title>Bistro Boss | Menu</title>
-            </Helmet>
-            This is menu page!
-        </div>
-    );
+  return (
+    <div>
+      <Helmet>
+        <title>Bistro Boss | Menu</title>
+      </Helmet>
+      <Cover img={menuImg} title="our menu"></Cover>
+      <PopularManu></PopularManu>
+      <Cover img={menuImg} title="our menu"></Cover>
+      <PopularManu></PopularManu>
+      <Cover img={menuImg} title="our menu"></Cover>
+      <PopularManu></PopularManu>
+      <Cover img={menuImg} title="our menu"></Cover>
+      <PopularManu></PopularManu>
+    </div>
+  );
 };
 
 export default Menu;
