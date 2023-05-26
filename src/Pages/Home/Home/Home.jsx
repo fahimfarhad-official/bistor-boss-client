59e80568203ee9e5a6d3f3735874c0bc18dd9951
Fahim Farhad <fahimfarhad@Fahims-MacBook-Air.local>
2023-05-26 09:57:06 +0600

    add dinamic title using react-helmet-async

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -4,10 +4,14 @@ import Category from '../Category/Category';
 import PopularManu from '../Popular Menu/PopularManu';
 import Featured from '../Featured/Featured';
 import TestiMonials from '../TestiMonials/TestiMonials';
+import { Helmet } from 'react-helmet-async';
 
 const Home = () => {
     return (
         <div>
+            <Helmet>
+                <title>Bistro Boss | Home</title>
+            </Helmet>
             <Banner></Banner>
             <Category></Category>
             <PopularManu></PopularManu>
